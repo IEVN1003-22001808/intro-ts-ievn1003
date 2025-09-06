68df0af59ece6195476de330e7486f78aff2b699
Jorge Izac Lara Cisneros <georgeizac.skul@gmail.com>
2025-09-05 18:46:59 -0600

    Segundo Commit 05/09/2025

---
+++ src/temasTS/01-tipos-bas.ts
@@ -0,0 +1,20 @@
+console.log("PAPU PAPU!")
+
+var num1 = 55;
+let num2 = 33;
+const num3 = 44;
+
+let nombre:string;
+let num:number;
+let acive:boolean;
+
+let sum = num1 + num2 + num3;
+
+console.log("el suma es: " + sum);
+
+let matricula:number | string;
+
+matricula = 12345;
+matricula = "PAPUPAPU";
+
+console.log(matricula);
